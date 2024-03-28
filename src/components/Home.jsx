63a406dd05a0499062fac4c15d9c0d9044750d56
painarasimha <pai.narasimha04@gmail.com>
2024-03-28 14:44:39 +0530

    HomePage Commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="container flex h-screen">
+      <img className="w-1/2 object-cover" src="src/assets/image4.jpg" alt="Website Image" />
+      <div className="text-content w-1/2 p-10">
+        <h1 className="text-3xl mb-10">Welcome to My Website!</h1>
+        <p className="text-lg mb-10">
+          This is a brief description of your website and what it offers. You can add more details about your services, products, or your company's mission. Make sure to keep the text concise and engaging.
+        </p>
+        <a href="#" className="text-white bg-green-500 py-2 px-4 rounded-md hover:bg-green-700">Get Started</a>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+const Navbar = () => {
+  const [searchOpen, setSearchOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchToggle = () => {
+    setSearchOpen(!searchOpen);
+  };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <nav className="bg-[#0b1f32] text-[#fff] font-bold flex justify-between items-center px-10 py-5">
+      <a href="/" className="text-xl font-bold">TravelEase</a>
+      <ul className="flex items-center space-x-4">
+        <div className="items-center">
+          <button
+            className="px-2 py-2 hover:bg-gray-600 rounded-full"
+            onClick={handleSearchToggle}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="white" class="w-5 h-5">
+              <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg>
+          </button>
+          {searchOpen && (
+            <input
+              type="text"
+              className="rounded-full border border-gray-300 focus:outline-none focus:ring focus:ring-blue-500 placeholder-gray-400"
+              placeholder=" Search..."
+              value={searchTerm}
+              onChange={handleChange}
+            />
+          )}
+        </div>
+        <li>
+          <a href="#" className="hover:text-gray-400">Home</a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-400">Travel Guide</a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-400">Hotels</a>
+        </li>
+        <li>
+          <a href="#" className="hover:text-gray-400">
+            <svg class="h-8 w-8 text-[#fdfdff]-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            <i className="fas fa-caret-down"></i>
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
