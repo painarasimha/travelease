eca5cf0337dceeb5899e22f39ced4e40a24f8726
painarasimha <pai.narasimha04@gmail.com>
2024-04-04 22:31:13 +0530

    Profile Page Update

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,76 +1,29 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import Img5 from '../assets/image1.jpg';
+import ProfileImg from '../assets/profile.jpg'
 
-function ProfilePage({ firstName = '', lastName = '', email = '', username = '', mobileNumber = '' }) {
-  const [userInfo, setUserInfo] = useState({
-    firstName,
-    lastName,
-    email,
-    username,
-    mobileNumber,
-  });
+function ProfilePage({ firstName = 'Narasimha', lastName = 'Pai', badgeName = 'Beginner Badge'}) {
+  const [progress, setProgress] = useState(73);
 
-  const handleInputChange = (event) => {
-    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
+  const handleProgressChange = (event) => {
+    const newProgress = parseInt(event.target.value);
+     const validProgress = Math.min(Math.max(newProgress, 0), 100);
   };
-
+  
   return (
-    <div className="profile-container bg-black bg-cover bg-center">
-      <div className="profile-page">
-        <hr className="border border-gray-200" />
-        <h1 className="text-4xl font-sans text-white text-center">Profile</h1>
-        <hr className="border border-gray-200" />
-
-        <div className="flex items-center mb-8">
-          <img src="https://via.placeholder.com/100" alt="img description" className="rounded-full w-48 h-48 mx-auto" />
-          <div className="user-info-container px-8 py-4 ml-4 bg-gray-100 rounded-lg">
-            <input
-              type="text"
-              className="w-full h-10 px-4 py-2 rounded-md text-center border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
-              name="firstName"
-              value={userInfo.firstName}
-              onChange={handleInputChange}
-              placeholder="Enter your First Name"
-            />
-            <br />
-            <input
-              type="text"
-              className="w-full h-10 px-4 py-2 rounded-md text-center border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
-              name="lastName"
-              value={userInfo.lastName}
-              onChange={handleInputChange}
-              placeholder="Enter your Last Name"
-            />
-            <br />
-            <input
-              type="text"
-              className="w-full h-10 px-4 py-2 rounded-md text-center border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
-              name="username"
-              value={userInfo.username}
-              onChange={handleInputChange}
-              placeholder="Enter your Username"
-            />
-            <br />
-            <input
-              type="email"
-              className="w-full h-10 px-4 py-2 rounded-md text-center border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
-              name="email"
-              value={userInfo.email}
-              onChange={handleInputChange}
-              placeholder="Enter your Email"
-            />
-            <br />
-            <input
-              type="text"
-              className="w-full h-10 px-4 py-2 rounded-md text-center border border-gray-300 focus:outline-none focus:ring-1 focus:ring-blue-500"
-              name="mobileNumber"
-              value={userInfo.mobileNumber}
-              onChange={handleInputChange}
-              placeholder="Enter your Mobile Number"
-            />
+    <div className="profile-container h-screen bg-fixed bg-cover px-8 pt-20 pb-8 " style={{ backgroundImage: `url(${Img5})` }}>
+      <div className="overlay fixed top-[calc(100px + 1rem)] left-1/2 transform -translate-x-1/2 py-8 px-12 rounded-md w-[1160px] h-[520px] bg-gray-300 opacity-70"></div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center -left-1/2">
+        <img src={ProfileImg} alt="Profile Picture" className="w-64 h-64 fixed rounded-full mb-4 border-2 border-black" />
+        <h1 className="text-2xl font-black mt-72 fixed text-black">{firstName} {lastName}</h1>
+        <div className="badge-container flex flex-col items-center fixed">
+          <span className="text-base font-bold mt-[396px]  text-gray-800">{badgeName}</span>
+          <div className="progress-bar bg-gray-200 h-2 rounded-full overflow-hidden w-full mt-2">
+            {/* Progress bar with dynamic width */}
+            <div className="progress bg-green-700 h-full rounded-full" style={{ width: `${progress}%` }}></div>
           </div>
+          <span className="text-sm text-black font-bold mt-1">{100-progress}% to Next Level</span>
         </div>
-        <button className="log-out-button text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-400 font-medium rounded-lg text-sm px-5 py-2.5 mr-auto mt-4">Log Out</button>
-        <hr className="border border-gray-200" />
       </div>
     </div>
   );
