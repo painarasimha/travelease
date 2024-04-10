6c29dfece35b403df9ce1d65014bbf247e3bcdad
painarasimha <pai.narasimha04@gmail.com>
2024-04-10 22:23:41 +0530

    GuidePage1

--- src/components/GuidePag1.jsx
+++ src/components/GuidePag1.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import Img2 from '../assets/image5.jpg'
+import leftarrow from '../assets/leftarrow.svg'
 import GuideProfile1 from '../assets/profile.jpg'
+import { useNavigate } from 'react-router-dom';
 
 function GuidePag1({firstName = 'Narasimha', lastName = 'Pai'}) {
+  const navigate = useNavigate();
+
+  const handleLeftArrow = () =>{
+    navigate('/travelpage')
+  }
   return (
     <div className="h-screen bg-cover bg-no-repeat bg-center py-14 px-[130px]" style={{ backgroundImage: `url(${Img2})` }}>
         <div className='w-[1260px] h-[590px] fixed bg-[#d7baad] rounded-3xl shadow-inner shadow-black py-8'>
-          <button className='ml-4 hover:'>
-            <svg width="30" height="30" viewBox="0 0 16 16">
-              <path d="M11 8H5L8 5l3-3 1 1L6 8z" /> {/* SVG path for a left arrow icon */}
-            </svg>
-          </button>
-          <div className='w-[835px] h-[530px] bg-[#F5F5DC] ml-[380px] rounded-xl p-4 border-2 border-black shadow-sm shadow-black '>
+          <div className='w-[835px] h-[530px] bg-[#F5F5DC] ml-[380px] rounded-xl p-4 border-2 border-black shadow-sm shadow-black overflow-y-auto'>
             <h1 className='text-2xl font-bold'>Description</h1>
             <p className='text-lg'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi in adipisci animi maiores incidunt aut ratione eius cum 
               porro fugit tempora vel quae, quia quam suscipit iste quod dolorum? Cupiditate.</p>
@@ -50,6 +52,9 @@ function GuidePag1({firstName = 'Narasimha', lastName = 'Pai'}) {
             <img src={GuideProfile1} alt="guide1profile" className='w-48 h-48 fixed rounded-full border-4 border-black'/>
             <h1 className="text-2xl font-black fixed text-black mt-52 ml-4">{firstName} {lastName}</h1>
             <div className='w-[2px] fixed h-[590px] ml-64 mt-[-60px] bg-black'></div>
+            <button className='fixed -mt-10 -ml-14 hover:scale-150 rounded ' onClick={handleLeftArrow}>
+              <img src={leftarrow} className='w-8 h-8 rounded'/>
+            </button>
         </div>   
     </div>
     
