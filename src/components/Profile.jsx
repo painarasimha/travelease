d3b1f0ef5824ff894f2b7ccb5f33f552f0a1f5fe
painarasimha <pai.narasimha04@gmail.com>
2024-05-02 04:09:21 +0530

    Profile Page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
 import Img5 from '../assets/image1.jpg';
-import ProfileImg from '../assets/profile1.jpg'
+import ProfileImg from '../assets/profile2.jpg'
 import { getAuth, signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
-function ProfilePage({ firstName = 'Narasimha', lastName = 'Pai', badgeName = 'Beginner Badge'}) {
+function ProfilePage({ firstName = 'Liam', lastName = 'Singh', email = 'liamsingh777@gmail.com', badgeName = 'Beginner Badge', phone = '8762283051'}) {
   const [progress, setProgress] = useState(57);
 
   const navigate = useNavigate();
@@ -34,7 +34,7 @@ function ProfilePage({ firstName = 'Narasimha', lastName = 'Pai', badgeName = 'B
     <div className="profile-container h-screen bg-cover px-8 pt-20 pb-8 " style={{ backgroundImage: `url(${Img5})` }}>
       <div className="overlay fixed top-[calc(40px + 1rem)] left-1/2 transform -translate-x-1/2 py-8 px-12 rounded-3xl shadow-md shadow-black w-[800px] h-[480px] bg-[#7d8321]"></div>
       <div className="absolute inset-0 flex flex-col items-center -left-1/2">
-        <img src={ProfileImg} alt="Profile Picture" className="mt-[200px] w-56 h-56 fixed ml-72 rounded-full mb-4 border-2 border-black" />
+        <img src={ProfileImg} alt="Profile Picture" className="mt-[200px] w-56 h-56 fixed ml-72 rounded-full mb-4 border-2 border-black object-cover" />
         <div className="badge-container ml-[300px] flex flex-col items-center fixed">
           <span className="text-base font-bold mt-[440px]  text-white">{badgeName}</span>
           <div className="progress-bar bg-gray-200 h-2 rounded-full overflow-hidden w-full mt-2">
@@ -43,8 +43,11 @@ function ProfilePage({ firstName = 'Narasimha', lastName = 'Pai', badgeName = 'B
           </div>
           <span className="text-sm text-black font-bold mt-1">{100-progress}% to Next Level</span>
         </div>
-        <h1 className="text-2xl font-black mt-[250px] ml-[750px] fixed text-black">{firstName} {lastName}</h1>
-        <button className="fixed ml-[300px] mt-[530px] px-4 py-2 bg-green-900 shadow-inner shadow-slate-500 text-white rounded-md hover:bg-green-700 focus:outline-none" onClick={handleLogout}>
+        <h1 className="text-2xl font-semibold p-56 left-[460px] fixed text-black">Firstname: {firstName}</h1>
+        <h1 className="text-2xl font-semibold p-64 top-4 left-[428px] fixed text-black">Lastname: {lastName}</h1>
+        <h1 className="text-2xl font-semibold p-64 top-16 left-[428px] fixed text-black">Email: {email}</h1>
+        <h1 className="text-2xl font-semibold p-64 top-[113px] left-[428px] fixed text-black">Phone No: {phone}</h1> 
+        <button className="fixed font-bold left-[700px] mt-[530px] px-5 py-3 bg-green-900 shadow-inner shadow-slate-500 text-white rounded-md hover:bg-green-700 focus:outline-none" onClick={handleLogout}>
           Logout
         </button>
       </div>
